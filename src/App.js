009170e027be3955462d0e5eba65d41a21027ece
morriszin <myominzin@gmail.com>
2022-06-18 22:01:57 +1000

    + change to github api from flickr
    The flickr api has issue with cors and now github was used instead.

--- src/App.js
+++ src/App.js
@@ -1,69 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Header() {
   return (
     <div className="jumbotron col-10 offset-1">
-      <h1 className="display-4"> Photo Search </h1>
-      <p className="lead"> Search for photos of your favorite things! </p>
+      <h1 className="display-4"> User Search </h1>
+      <p className="lead"> Search your favorite github users! </p>
     </div>
   );
 }
 
-function Search(props) {
+function Search({ onShowProfile, onHideProfile }) {
+  const [searchStr, setSearchStr] = useState('');
+
+  async function getPhotos(id) {
+    try {
+      if (id) {
+        const resp = await axios.get(
+          `https://api.github.com/users/${searchStr}`
+        );
+        onShowProfile(resp.data);
+      } else {
+        onHideProfile();
+      }
+    } catch (err) {
+      console.log(err);
+      onHideProfile();
+    }
+  }
+
+  const handleChange = (e) => {
+    setSearchStr(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    getPhotos(e.target.value);
+  };
+
   return (
     <div className="row">
       <div className="col-10 offset-1">
         <nav className="navbar navbar-light bg-light">
-          <form className="form-inline">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </form>
+          <input
+            className="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchStr}
+            onChange={handleChange}
+            onKeyUp={handleSubmit}
+          />
         </nav>
       </div>
     </div>
   );
 }
 
-function Image({ image }) {
+function ImageList({ profiles }) {
+  return (
+    <div className="row">
+      {profiles.map((item) => (
+        <Image key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
+
+function Image(props) {
   return (
     <div className="row">
       <div className="col-4 offset-1">
-        <a href={image.link} target="_blank" rel="noreferrer">
-          <img src={image.media.m} alt="showing here" />
+        <a href={props.html_url} target="_blank" rel="noreferrer">
+          <img src={props.avatar_url} alt="avatar showing here" />
         </a>
       </div>
       <div className="col-6">
-        <h2>Author : {image.author} </h2>
-        <p> Date Taken : {image.date_taken} </p>
-        <p> Tags : {image.tags} </p>
+        <h2>User : {props.name} </h2>
+        <p> Login : {props.login} </p>
+        <p> Twitter : {props.twitter_username} </p>
+        <p> Created at : {props.created_at} </p>
       </div>
     </div>
   );
 }
 
-function Footer(props) {
-  return (
-    <div id="footer" className="row">
-      <div className="col-4 offset-1">
-        <p className="text-muted"> All the images are from flickr! </p>
-      </div>
-    </div>
-  );
-}
+function App() {
+  const [searchData, setSearchData] = useState({ profiles: [] });
 
-function App({ searchData }) {
+  function showProfiles(profileData) {
+    setSearchData(() => ({
+      profiles: [profileData],
+    }));
+  }
+
+  function hideProfiles() {
+    setSearchData(() => ({ profiles: [] }));
+  }
+
   return (
     <div className="container-fluid">
       <Header />
-      <Search />
-      <Image {...searchData} />
-      <Footer />
+      <Search onShowProfile={showProfiles} onHideProfile={hideProfiles} />
+      <ImageList {...searchData} />
     </div>
   );
 }
