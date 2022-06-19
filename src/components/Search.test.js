46217ecc246249cfb44f2e22c441fbef8e6a2a56
morriszin <myominzin@gmail.com>
2022-06-19 15:24:49 +1000

    + added event tests
    added function call with jest's mock

--- src/components/Search.test.js
+++ src/components/Search.test.js
@@ -13,4 +13,36 @@ describe('Search', () => {
       expect(search.props.placeholder).toBe('Search..');
     });
   });
+
+  describe('events', () => {
+    it('calls onChange', async () => {
+      const fn = jest.fn();
+      fn('test');
+
+      const tree = TestRenderer.create(<Search onChange={fn} />);
+      const search = await tree.root.findByProps({
+        'data-testid': 'search-input',
+      });
+      TestRenderer.act(() => {
+        search.props.onChange({ target: { value: 'test' } });
+      });
+
+      expect(fn.mock.calls).toEqual([['test']]);
+    });
+  });
+
+  it('calls onKeyUp', async () => {
+    const fn = jest.fn();
+    fn('test');
+
+    const tree = TestRenderer.create(<Search onChange={fn} />);
+    const search = await tree.root.findByProps({
+      'data-testid': 'search-input',
+    });
+    TestRenderer.act(() => {
+      search.props.onKeyUp({ target: { value: 'test' } });
+    });
+
+    expect(fn.mock.calls).toEqual([['test']]);
+  });
 });
